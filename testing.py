ea5ca8f12d15465a465df96a9258a00a5e0f3158
Shyam Parikh <65629263+sparikh099@users.noreply.github.com>
2023-01-20 12:31:07 -0500

    Update testing.py

--- testing.py
+++ testing.py
@@ -1189,6 +1189,14 @@ def main() -> None:
                 """
             )
     except:
+        st.markdown(
+                f"""
+                * Ticker Symbol : {specific.getTicker()}
+                * {specific.isSP500()}    
+                * Current Price : ${specific.getCurrPrice()}
+                * Company Info :  {specific.getCompanyInfo()}
+                * Articles ARE NOT AVAILABLE
+                """
         pass
     try:
         #Black-Scholes Model for Options Pricing
