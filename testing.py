523ee720c4c8c10c216ebd7420d122048c513a14
Shyam Parikh <65629263+sparikh099@users.noreply.github.com>
2023-10-03 14:04:38 -0400

    Update testing.py

--- testing.py
+++ testing.py
@@ -889,6 +889,7 @@ def main() -> None:
                 stockList.append(Stock(df['Symbol'][x]))
             except:
                 pass
+            st.sidebar.write(df['Symbol'][x])
         df = ProfitableSort(stockList,df).returnDf()  
 
     st.sidebar.subheader("Filter Displayed Accounts")
